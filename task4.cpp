a10f4b71b8bd0e3b65d6d7e9fc1200572d7c4d44
admirrama2 <154549193+admirrama2@users.noreply.github.com>
2024-05-05 22:55:35 +0200

    Create task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <concepts>
+
+//Create a template that use's a already existant concept that chekcs if a value is comperable to  a string
+//Use the template to write a function arePermutations that checks if two words are permutaions of eachother
+
+
+int main() {
+    std::string s1 = "hello";
+    std::string s2 = "world";
+    std::string s3 = "olleh";
+
+    std::cout << std::boolalpha;
+    std::cout << arePermutations(s1, s2) << std::endl; // false
+    std::cout << arePermutations(s1, s3) << std::endl; // true
+
+    const char* c1 = "hello";
+    const char* c2 = "world";
+    const char* c3 = "olleh";
+
+    std::cout << arePermutations(c1, c2) << std::endl; // false
+    std::cout << arePermutations(c1, c3) << std::endl; // true
+
+    return 0;
+}
