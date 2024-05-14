8fc4e97fa80b2a49b78ecd3336af855dbc57bfe0
admirrama2 <154549193+admirrama2@users.noreply.github.com>
2024-05-14 11:44:09 +0200

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -7,7 +7,7 @@
 template<typename T>
 //Create a concept that checks if two values are comperable(<,==,> etc.)
 //Use the concept to create a template and use it to create a simple function areComperable that prints a message if they are comparable
-
+//HINT: for them to be comparable any of above operations have to return true or false
 
 int main() {
 
